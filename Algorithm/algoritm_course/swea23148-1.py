e7f6e18c9b5e86a9bfa3dedfe9be4de6f56d4e20
ghfy1245.dev@gmail.com <ghfy1245.dev@gmail.com>
2025-02-06 20:37:54 +0900

    complete

---
+++ Algorithm/algoritm_course/swea23148-1.py
@@ -0,0 +1,24 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+# 노선 수 T
+T = int(input())
+
+def minimun_charge(arr, K, N, M):
+    bus = 0
+    moving = 0
+    while bus < N - K:
+        if :
+            bus = charger
+            moving += 1
+        else:
+            return 0
+    return moving
+
+for lane in range(1, T+1):
+    # 최대 이동거리 K, 전체 정류장 개수 N, 충전기 설치된 정류장 개수 M
+    K, N, M = map(int, input().split())
+    arr = set(map(int,input().split()))
+    print(f'#{lane} {minimun_charge(arr, K, N, M)}')
+
+
--- Algorithm/algoritm_course/swea23148.py
+++ Algorithm/algoritm_course/swea23148.py
@@ -5,13 +5,14 @@ sys.stdin = open("input.txt", "r")
 T = int(input())
 
 def minimun_charge(arr, K, N, M):
-    counts = [0] * (N+K) # 정류장 개수
+    counts = [0] * (N) # 정류장 개수
     for i in range(M): #
         counts[arr[i]] += 1 # counts = [0, 0, 1, 0, 0, 1, ...]
     moving = 0
     bus = 0
     while bus < N - K: # 종착역의 K 이내 거리만큼 도달 시 루프 중지
-        if 1 in set(counts[bus+1:bus+K+1]): # K 거리 내에 충전소가 있는지
+        distance = set(counts[bus + 1:bus + K + 1])
+        if 1 in distance: # K 거리 내에 충전소가 있는지
             bus = len(counts[0:bus+K+1]) - (counts[bus:bus+K+1][::-1].index(1) + 1) # 역순으로 인덱싱...
             moving +=1
         else:
