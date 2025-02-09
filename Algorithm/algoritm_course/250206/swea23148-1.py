67361d15cbb87952d9b427781d312b9c5af24e7d
ghfy1245.dev@gmail.com <ghfy1245.dev@gmail.com>
2025-02-09 17:23:00 +0900

    a

--- Algorithm/algoritm_course/250206/swea23148-1.py
+++ Algorithm/algoritm_course/250206/swea23148-1.py
@@ -13,7 +13,7 @@ def minimun_charge(arr, K, N, M):
             moving += 1
         else:
             return 0
-    return moving
+    return movingq
 
 for lane in range(1, T+1):
     # 최대 이동거리 K, 전체 정류장 개수 N, 충전기 설치된 정류장 개수 M
