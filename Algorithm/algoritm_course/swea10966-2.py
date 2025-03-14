c336102d733c9677cbb572f17585bb5dd3afb976
EdwardLee <ghfy1245.dev@gmail.com>
2025-03-15 00:27:30 +0900

    algoritm

--- Algorithm/algoritm_course/swea10966-2.py
+++ Algorithm/algoritm_course/swea10966-2.py
@@ -1,49 +1,44 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+delta = [(1,0), (0,1), (-1,0), (0,-1)]
+
 from collections import deque
-
-delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
 
 def get_dist(arr, p, N, M):
     y, x = p
     i, j = p
     que = deque()
-    Mu = []
+    visited = []
     while True:
         for di, dj in delta:
             ni = i + di
             nj = j + dj
-            if 0 <= ni < N and 0 <= nj < M:
-                if abs(y - i) + abs(x - j) > 1 and arr[ni][nj] == "W":
-                    return
+            if 0 <= ni < N and 0 <= nj < M and (ni, nj) not in visited:
                 if arr[ni][nj] == "L":
                     if arr_1[ni][nj] == 0 or arr_1[ni][nj] > abs(y - ni) + abs(x - nj):
-                        Mu.append( (ni, nj))
                         arr_1[ni][nj] = abs(y - ni) + abs(x - nj)
+                        que.append((ni, nj))
+                        visited.append((ni, nj))
         if que:
             i, j = que.popleft()
         else:
-            que.extend(Mu)
-            Mu = []
-            if que:
-                i, j = que.popleft()
-            else:
-                return
+            return
+
+
 T = int(input())
 
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     arr = [input() for _ in range(N)]
+    arr_1 = [[0] * M for _ in range(N)]
     total = 0
-    arr_1 = [[0]*M for _ in range(N)]
     for i in range(N):
         for j in range(M):
             if arr[i][j] == "W":
                 get_dist(arr, (i,j), N, M)
-    for row in arr_1:
-        print(*row)
+    # for row in arr_1:
+    #     print(*row)
     for row in arr_1:
         total += sum(row)
     print(f'#{tc} {total}')
