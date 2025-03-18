da45d28b8375c5a03b9dde5e88bc01a6730f1eff
EdwardLee <ghfy1245.dev@gmail.com>
2025-03-18 21:49:13 +0900

    update

--- Algorithm/algoritm_course/swea5209.py
+++ Algorithm/algoritm_course/swea5209.py
@@ -1,7 +1,27 @@
-
+from pydoc import visiblename
 
 T = int(input())
 
 for tc in range(1, T+1):
     N = int(input())
-    arr = [list(map(int, input().split()))]
+    arr = []
+    for _ in range(N):
+        arr.extend(list(map(int, input().split())))
+    arr_p = []
+    for i in range(N):
+        for j in range(N):
+            arr_p.append((i,j))
+    cost = 0
+    cnt = 0
+    goods = list(range(N))
+    factory = []
+    while cnt < N:
+        min_idx = arr.index(min(arr))
+        g, f = arr_p.pop(min_idx)
+        c = arr.pop(min_idx)
+        if f not in factory and g in goods:
+            cost += c
+            factory.append(f)
+            goods.remove(g)
+            cnt += 1
+    print(f'#{tc} {cost}')
