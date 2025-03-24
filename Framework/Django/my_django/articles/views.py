4ba8508822c41a31b222dafc884e6d49c2499e2d
EdwardLee <ghfy1245.dev@gmail.com>
2025-03-25 08:49:35 +0900

    django

--- Framework/Django/my_django/articles/views.py
+++ Framework/Django/my_django/articles/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
-def index(request):    
+def index(request):
+    context = {
+        'name': 'napoie',
+    }
     # 메인 페이지 응답 (request) = 약속 객체 반환
-    return render(request, 'articles/index.html')
+    return render(request, 'articles/index.html', context)
+
+def digimon(request):
+    partner_digimons = ['아구', '파피', '팔', '피요', '파닥', '쉬라', '텐타']
+    partners = {
+        '아구': '태일',
+        '파피': '메튜',
+    }
+    picked = random.choice(partner_digimons)
+    if picked not in ['아구', '파피']:
+        partners[picked] = '조연'
+    context = {
+        'who': partner_digimons,
+        'digimon': picked,
+        'partner': partners[picked]
+    }
+    return render(request, 'articles/digimon.html', context)
+
+def search(request):
+    return render(request, 'articles/search.html')
--- Framework/Django/my_django/firstpjt/urls.py
+++ Framework/Django/my_django/firstpjt/urls.py
@@ -21,4 +21,6 @@ from articles import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', views.index),
+    path('digimons/', views.digimon),
+    path('search/', views.search),
 ]
