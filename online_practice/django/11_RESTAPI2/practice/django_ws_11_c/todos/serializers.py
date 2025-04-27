3a8ecb63764f76e4ccc0a515d8cfe853156ebb11
EdwardLee <ghfy1245.dev@gmail.com>
2025-04-27 22:02:17 +0900

    onlint

---
+++ online_practice/django/11_RESTAPI2/practice/django_ws_11_c/todos/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Todo, Recommend
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+class TodoListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('work', 'is_completed', )
+
+class RecommendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recommend
+        fields = '__all__'
+        read_only_fields = ('todo',)
